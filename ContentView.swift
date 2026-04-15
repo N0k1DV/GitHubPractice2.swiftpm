60cd137ca86e0e95e7362409b71cdcd88672d08e
Niko D <moki.gaming12@gmail.com>
2026-04-15 10:36:57 -0500

    Changed "Enter number 1" to "Enter number 2"

--- ContentView.swift
+++ ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     @State var number2 = 0
     var body: some View {
         
-        TextField("Enter number 1", value: $number2, format: .number)
+        TextField("Enter number 2", value: $number2, format: .number)
         
     }
 }
