c07299717c00f77985063137551c6a9c136ab7bd
AlexP <alikpritsker@gmail.com>
2026-04-15 10:30:31 -0500

    Created a @State variable to store a number. Called it number2.

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number2 = 0
     var body: some View {
-       
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
     }
 }
